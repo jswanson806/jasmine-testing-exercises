3a7f37ae421640400db0851ade0959fff9d130d5
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-08-22 06:18:16 -0500

    adding completed exercise code

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,9 @@
-
+//check the returned result from initial values
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment(getCurrentUIValues())).toEqual('665.30');
 });
 
-
+//split the returned string at the decimal and check the remaining length
 it("should return a result with 2 decimal places", function() {
-  // ..
+ expect(calculateMonthlyPayment(getCurrentUIValues()).split(".").length).toEqual(2);
 });
-
-/// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,36 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const loanAmount = document.getElementById("loan-amount");
+  const loanYears = document.getElementById("loan-years");
+  const loanRate = document.getElementById("loan-rate");
+  loanAmount.value = 100000;
+  loanYears.value = 30;
+  loanRate.value = 7;
+  calculateMonthlyPayment(getCurrentUIValues());
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const monthly = calculateMonthlyPayment(getCurrentUIValues());
+  updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const p = values.amount;
+  const i = (values.rate / 100) / 12;
+  const n = values.years * 12;
+  let monthly = ((p * i) / (1 - Math.pow((1 + i), -n))).toFixed(2);
+  return monthly;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyOutput = document.getElementById('monthly-payment');
+  monthlyOutput.innerText = monthly;
 }

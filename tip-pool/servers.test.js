e2ca16383126906838c5ac7da8caf7d4b1893afc
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-08-23 15:35:00 -0500

    first set of jasmine tests complete

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,18 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should update table with new server and tip amount', function () {
+    submitServerInfo();
+    
+     expect(serverTbody.innerHTML).not.toEqual('');
+     expect(document.getElementById('key')).toBeDefined();
+     
+    });
+
   afterEach(function() {
     // teardown logic
+    allServers['server' + serverId] = {};
+    serverTbody.innerHTML = '';
   });
 });
+
